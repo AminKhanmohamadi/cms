29b7ce894c66dd78bc0c474d82bae5ee1b2d21a9
amin <aminkhm097@gmail.com>
2025-01-21 21:36:24 +0330

    fix template url and ...

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth import get_user_model
 class Category(models.Model):
     title = models.CharField(max_length=255 , verbose_name=_('title'))
     active = models.BooleanField(default=True, verbose_name=_('active'))
+    thumbnail = models.ImageField(upload_to="images/", verbose_name=_('thumbnail') , null=True)
 
     def __str__(self):
         return self.title
